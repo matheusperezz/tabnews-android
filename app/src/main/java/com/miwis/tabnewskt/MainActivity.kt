ca33691881df25752c4ebaf63eb9193301ee0490
matheusperezz <matheus.mperez08@gmail.com>
2023-12-28 12:39:18 -0300

    refactor: refactor tabnewsapp in mainactivity

--- app/src/main/java/com/miwis/tabnewskt/MainActivity.kt
+++ app/src/main/java/com/miwis/tabnewskt/MainActivity.kt
@@ -23,6 +23,7 @@ import androidx.navigation.NavHostController
 import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
 import com.miwis.tabnewskt.ui.components.BottomBarNavigation
+import com.miwis.tabnewskt.ui.components.TabnewsApp
 import com.miwis.tabnewskt.ui.components.TabnewsKtApp
 import com.miwis.tabnewskt.ui.components.TabnewsKtFab
 import com.miwis.tabnewskt.ui.components.TabnewsKtTopBar
@@ -59,35 +60,7 @@ class MainActivity : ComponentActivity() {
       val currentDestination = navBackStackEntry?.destination
 
       TabnewsKtTheme {
-        Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-
-          val currentRoute = currentDestination?.route
-
-          val isShowBottomBar = when (currentRoute) {
-            relevantTabsListRoute, newTabsRoute, settingsRoute, postDetailsRoute -> true
-            else -> false
-          }
-
-          val isShowTopBar = when (currentRoute) {
-            loginRoute -> false
-            else -> true
-          }
-
-          val isShowFab = when (currentRoute) {
-            loginRoute, newPostRoute, postDetailsRoute -> false
-            else -> true
-          }
-
-          TabnewsKtApp(
-            navController = navController,
-            currentDestination = currentDestination,
-            isShowBottomBar = isShowBottomBar,
-            isShowTopBar = isShowTopBar,
-            isShowFab = isShowFab,
-          ) {
-            TabnewsNavHost(navController = navController)
-          }
-        }
+        TabnewsApp(currentDestination, navController)
       }
     }
   }
